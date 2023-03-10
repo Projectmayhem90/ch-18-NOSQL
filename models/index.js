ccc4f05889c1f74228e9ded29d99b33c61a76f6d
JoshNatYoyo <joshuaobrien28@yahoo.com>
2023-03-09 22:29:47 -0500

    Updated models

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-const User = require('./User');
-const Thought = require('./Thought');
+const User = require('./User.js');
+const Course = require('./Course.js');
 
-module.exports = { User, Thought };
+module.exports = { User, Course };
